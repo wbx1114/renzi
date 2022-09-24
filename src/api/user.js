ea77ba5b5a33305d1163273abe4a148c22353180
ice <2190753551@qq.com>
2022-09-24 09:22:13 +0800

    'feat:项目初始化'

---
+++ src/api/user.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+
+  })
+}
+
+export function getInfo(token) {
+
+}
+
+export function logout() {
+
+}
---
+++ src/utils/request.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+const service = axios.create({
+  baseUrl: '',
+  timeout: 5000
+})
+
+export default service
